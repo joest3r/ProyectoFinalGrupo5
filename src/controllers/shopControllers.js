0d01bff48e2b48adfd1326196a13ee06cb601621
jo3st3r <fgluiv@gmail.com>
2023-12-15 20:53:32 -0300

    FEATURE: Renderizado de imagenes en carousel y shop

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,8 +1,9 @@
-const fs = require("fs");
-const funkoShopJSON = JSON.parse(fs.readFileSync("../../funkoshop.json"));
+const fs = require("fs"); 
+const { title } = require("process");
+const funkoJSON = JSON.parse(fs.readFileSync("./src/database/funkoshop.json","utf-8")); 
 
 module.exports = {
-    shop: (req,res) => { res.render("shop",{title:"Shop | FunkoShop"})},
+    shop: (req,res) => { res.render("shop",{title:"Shop | FunkoShop",funko:funkoJSON})},
     //item: (req,res) => {res.render("item",{title:"Item | FunkoShop"})},
     //itemPOST: (req,res) => {res.render("item",{title:"Item | FunkoShop"})},
     //carritoPOST: (req,res) => {res.render('carrito",{title:"Carrito | FunkoShop')},
